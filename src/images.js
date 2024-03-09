6ea62a254976b25c0d3836296281923d9c8cb8bf
Eren Çolak <93825099+erenxcolakx@users.noreply.github.com>
2024-03-10 00:21:18 +0300

    created for importing img files

---
+++ src/images.js
@@ -0,0 +1,226 @@
+import M1 from './assets/MessierObjects/Messier-1.webp';
+import M2 from './assets/MessierObjects/Messier-2.webp';
+import M3 from './assets/MessierObjects/Messier-3.webp';
+import M4 from './assets/MessierObjects/Messier-4.webp';
+import M5 from './assets/MessierObjects/Messier-5.webp';
+import M6 from './assets/MessierObjects/Messier-6.webp';
+import M7 from './assets/MessierObjects/Messier-7.webp';
+import M8 from './assets/MessierObjects/Messier-8.webp';
+import M9 from './assets/MessierObjects/Messier-9.webp';
+import M10 from './assets/MessierObjects/Messier-10.webp';
+import M11 from './assets/MessierObjects/Messier-11.webp';
+import M12 from './assets/MessierObjects/Messier-12.webp';
+import M13 from './assets/MessierObjects/Messier-13.webp';
+import M14 from './assets/MessierObjects/Messier-14.webp';
+import M15 from './assets/MessierObjects/Messier-15.webp';
+import M16 from './assets/MessierObjects/Messier-16.webp';
+import M17 from './assets/MessierObjects/Messier-17.webp';
+import M18 from './assets/MessierObjects/Messier-18.webp';
+import M19 from './assets/MessierObjects/Messier-19.webp';
+import M20 from './assets/MessierObjects/Messier-20.webp';
+import M21 from './assets/MessierObjects/Messier-21.webp';
+import M22 from './assets/MessierObjects/Messier-22.webp';
+import M23 from './assets/MessierObjects/Messier-23.webp';
+import M24 from './assets/MessierObjects/Messier-24.webp';
+import M25 from './assets/MessierObjects/Messier-25.webp';
+import M26 from './assets/MessierObjects/Messier-26.webp';
+import M27 from './assets/MessierObjects/Messier-27.webp';
+import M28 from './assets/MessierObjects/Messier-28.webp';
+import M29 from './assets/MessierObjects/Messier-29.webp';
+import M30 from './assets/MessierObjects/Messier-30.webp';
+import M31 from './assets/MessierObjects/Messier-31.webp';
+import M32 from './assets/MessierObjects/Messier-32.webp';
+import M33 from './assets/MessierObjects/Messier-33.webp';
+import M34 from './assets/MessierObjects/Messier-34.webp';
+import M35 from './assets/MessierObjects/Messier-35.webp';
+import M36 from './assets/MessierObjects/Messier-36.webp';
+import M37 from './assets/MessierObjects/Messier-37.webp';
+import M38 from './assets/MessierObjects/Messier-38.webp';
+import M39 from './assets/MessierObjects/Messier-39.webp';
+import M40 from './assets/MessierObjects/Messier-40.webp';
+import M41 from './assets/MessierObjects/Messier-41.webp';
+import M42 from './assets/MessierObjects/Messier-42.webp';
+import M43 from './assets/MessierObjects/Messier-43.webp';
+import M44 from './assets/MessierObjects/Messier-44.webp';
+import M45 from './assets/MessierObjects/Messier-45.webp';
+import M46 from './assets/MessierObjects/Messier-46.webp';
+import M47 from './assets/MessierObjects/Messier-47.webp';
+import M48 from './assets/MessierObjects/Messier-48.webp';
+import M49 from './assets/MessierObjects/Messier-49.webp';
+import M50 from './assets/MessierObjects/Messier-50.webp';
+import M51 from './assets/MessierObjects/Messier-51.webp';
+import M52 from './assets/MessierObjects/Messier-52.webp';
+import M53 from './assets/MessierObjects/Messier-53.webp';
+import M54 from './assets/MessierObjects/Messier-54.webp';
+import M55 from './assets/MessierObjects/Messier-55.webp';
+import M56 from './assets/MessierObjects/Messier-56.webp';
+import M57 from './assets/MessierObjects/Messier-57.webp';
+import M58 from './assets/MessierObjects/Messier-58.webp';
+import M59 from './assets/MessierObjects/Messier-59.webp';
+import M60 from './assets/MessierObjects/Messier-60.webp';
+import M61 from './assets/MessierObjects/Messier-61.webp';
+import M62 from './assets/MessierObjects/Messier-62.webp';
+import M63 from './assets/MessierObjects/Messier-63.webp';
+import M64 from './assets/MessierObjects/Messier-64.webp';
+import M65 from './assets/MessierObjects/Messier-65.webp';
+import M66 from './assets/MessierObjects/Messier-66.webp';
+import M67 from './assets/MessierObjects/Messier-67.webp';
+import M68 from './assets/MessierObjects/Messier-68.webp';
+import M69 from './assets/MessierObjects/Messier-69.webp';
+import M70 from './assets/MessierObjects/Messier-70.webp';
+import M71 from './assets/MessierObjects/Messier-71.webp';
+import M72 from './assets/MessierObjects/Messier-72.webp';
+import M73 from './assets/MessierObjects/Messier-73.webp';
+import M74 from './assets/MessierObjects/Messier-74.webp';
+import M75 from './assets/MessierObjects/Messier-75.webp';
+import M76 from './assets/MessierObjects/Messier-76.webp';
+import M77 from './assets/MessierObjects/Messier-77.webp';
+import M78 from './assets/MessierObjects/Messier-78.webp';
+import M79 from './assets/MessierObjects/Messier-79.webp';
+import M80 from './assets/MessierObjects/Messier-80.webp';
+import M81 from './assets/MessierObjects/Messier-81.webp';
+import M82 from './assets/MessierObjects/Messier-82.webp';
+import M83 from './assets/MessierObjects/Messier-83.webp';
+import M84 from './assets/MessierObjects/Messier-84.webp';
+import M85 from './assets/MessierObjects/Messier-85.webp';
+import M86 from './assets/MessierObjects/Messier-86.webp';
+import M87 from './assets/MessierObjects/Messier-87.webp';
+import M88 from './assets/MessierObjects/Messier-88.webp';
+import M89 from './assets/MessierObjects/Messier-89.webp';
+import M90 from './assets/MessierObjects/Messier-90.webp';
+import M91 from './assets/MessierObjects/Messier-91.webp';
+import M92 from './assets/MessierObjects/Messier-92.webp';
+import M93 from './assets/MessierObjects/Messier-93.webp';
+import M94 from './assets/MessierObjects/Messier-94.webp';
+import M95 from './assets/MessierObjects/Messier-95.webp';
+import M96 from './assets/MessierObjects/Messier-96.webp';
+import M97 from './assets/MessierObjects/Messier-97.webp';
+import M98 from './assets/MessierObjects/Messier-98.webp';
+import M99 from './assets/MessierObjects/Messier-99.webp';
+import M100 from './assets/MessierObjects/Messier-100.webp';
+import M101 from './assets/MessierObjects/Messier-101.webp';
+import M102 from './assets/MessierObjects/Messier-102.webp';
+import M103 from './assets/MessierObjects/Messier-103.webp';
+import M104 from './assets/MessierObjects/Messier-104.webp';
+import M105 from './assets/MessierObjects/Messier-105.webp';
+import M106 from './assets/MessierObjects/Messier-106.webp';
+import M107 from './assets/MessierObjects/Messier-107.webp';
+import M108 from './assets/MessierObjects/Messier-108.webp';
+import M109 from './assets/MessierObjects/Messier-109.webp';
+import M110 from './assets/MessierObjects/Messier-110.webp';
+
+
+const IMAGES = {
+    M1: M1,
+    M2: M2,
+    M3: M3,
+    M4: M4,
+    M5: M5,
+    M6: M6,
+    M7: M7,
+    M8: M8,
+    M9: M9,
+    M10: M10,
+    M11: M11,
+    M12: M12,
+    M13: M13,
+    M14: M14,
+    M15: M15,
+    M16: M16,
+    M17: M17,
+    M18: M18,
+    M19: M19,
+    M20: M20,
+    M21: M21,
+    M22: M22,
+    M23: M23,
+    M24: M24,
+    M25: M25,
+    M26: M26,
+    M27: M27,
+    M28: M28,
+    M29: M29,
+    M30: M30,
+    M31: M31,
+    M32: M32,
+    M33: M33,
+    M34: M34,
+    M35: M35,
+    M36: M36,
+    M37: M37,
+    M38: M38,
+    M39: M39,
+    M40: M40,
+    M41: M41,
+    M42: M42,
+    M43: M43,
+    M44: M44,
+    M45: M45,
+    M46: M46,
+    M47: M47,
+    M48: M48,
+    M49: M49,
+    M50: M50,
+    M51: M51,
+    M52: M52,
+    M53: M53,
+    M54: M54,
+    M55: M55,
+    M56: M56,
+    M57: M57,
+    M58: M58,
+    M59: M59,
+    M60: M60,
+    M61: M61,
+    M62: M62,
+    M63: M63,
+    M64: M64,
+    M65: M65,
+    M66: M66,
+    M67: M67,
+    M68: M68,
+    M69: M69,
+    M70: M70,
+    M71: M71,
+    M72: M72,
+    M73: M73,
+    M74: M74,
+    M75: M75,
+    M76: M76,
+    M77: M77,
+    M78: M78,
+    M79: M79,
+    M80: M80,
+    M81: M81,
+    M82: M82,
+    M83: M83,
+    M84: M84,
+    M85: M85,
+    M86: M86,
+    M87: M87,
+    M88: M88,
+    M89: M89,
+    M90: M90,
+    M91: M91,
+    M92: M92,
+    M93: M93,
+    M94: M94,
+    M95: M95,
+    M96: M96,
+    M97: M97,
+    M98: M98,
+    M99: M99,
+    M100: M100,
+    M101: M101,
+    M102: M102,
+    M103: M103,
+    M104: M104,
+    M105: M105,
+    M106: M106,
+    M107: M107,
+    M108: M108,
+    M109: M109,
+    M110: M110,
+};
+
+export default IMAGES;
