29bfc2fb560ab2fcd3784e60af7f8a871f75f698
Eren Çolak <93825099+erenxcolakx@users.noreply.github.com>
2024-03-18 22:15:38 +0300

    added func. to create messier card

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,25 @@ import IMAGES from './images';
 
 const messierObjects = messierData.messier_objects;
 
+function createMessierCard(item) {
+  return(
+    <MessierCard
+      key={item.name}
+      name={item.name}
+      commonName={item.common_name}
+      type={item.type}
+      constellation={item.constellation}
+      src={IMAGES[item.name]}
+    />
+  );
+}
+
 function App() {
   return (
     <div className='gradient-background'>
       <Header />
       <div className='container d-flex flex-wrap justify-content-center gap-3 my-3'>
-        {messierObjects.map((item) => (
-          <MessierCard
-            key={item.name}
-            name={item.name}
-            commonName={item.common_name}
-            type={item.type}
-            constellation={item.constellation}
-            src={IMAGES[item.name]}
-          />
-        ))}
+        {messierObjects.map(createMessierCard)}
       </div>
     </div>
   );
